d6d36e827c8bd8e19d41df1f046f2c56e55b19f4
nizamov4 <ireknizamov94@gmail.com>
2022-08-25 22:39:38 +0300

    HW 1 REACT

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from './components/card.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <main className='main'>
+      <div className='container'>
+      <header>
+      <h1 className='header'>НАША ПРОДУКЦИЯ</h1>
+      <div className="basket">
+        <p className='basket__desc'> 3 товара
+на сумму 3 500 ₽
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img className='add'src="./img/basket.png" alt="" />
+      </div>
       </header>
-    </div>
+      <div className='menu'>
+
+      <Card
+      url={'img/1.png'}
+      tittle={'Устрицы по рокфеллеровски'}
+      description={'Значимость этих проблем настолько очевидна, что укрепление и развитие структуры '}
+      price={'2 700 ₽/ 500 г.'}
+      urlIcon={'img/pluse_white.png'}
+      />
+
+      <Card
+      url={'img/2.png'}
+      tittle={'Свиные ребрышки на гриле с зеленью'}
+      description={'Не следует, однако забывать, что реализация намеченных плановых, что укрепление '}
+      price={'1 600 ₽/ 750 г.'}
+      urlIcon={'img/pluse_white.png'}
+      />
+
+      <Card
+      url={'img/3.png'}
+      tittle={'Свиные ребрышки на гриле с зеленью'}
+      description={'Не следует, однако забывать, что реализация намеченных плановых, что укрепление'}
+      price={'2 700 ₽/ 500 г.'}
+      urlIcon={'img/pluse_white.png'}
+      />
+
+      <Card
+      url={'img/4.png'}
+      tittle={'Устрицы по рокфеллеровски'}
+      description={'Значимость этих проблем настолько очевидна, что укрепление и развитие структуры '}
+      price={'2 700 ₽/ 500 г.'}
+      urlIcon={'img/pluse_white.png'}
+      />
+
+      <Card
+      url={'img/8.png'}
+      tittle={'Устрицы по рокфеллеровски'}
+      description={'Значимость этих проблем настолько очевидна, что укрепление и развитие структуры '}
+      price={'2 700 ₽/ 500 г.'}
+      urlIcon={'img/pluse_white.png'}
+      />
+
+      <Card
+      url={'img/6.png'}
+      tittle={'Свиные ребрышки на гриле с зеленью'}
+      description={'Не следует, однако забывать, что реализация намеченных плановых, что укрепление'}
+      price={'1 600 ₽/ 750 г.'}
+      urlIcon={'img/pluse_white.png'}
+      />
+
+      <Card
+      url={'img/7.png'}
+      tittle={'Свиные ребрышки на гриле с зеленью'}
+      description={'Не следует, однако забывать, что реализация намеченных плановых, что укрепление'}
+      price={'2 700 ₽/ 500 г.'}
+      urlIcon={'img/pluse_white.png'}
+      />
+
+      <Card
+      url={'img/5.png'}
+      tittle={'Устрицы по рокфеллеровски'}
+      description={'Значимость этих проблем настолько очевидна, что укрепление и развитие структуры '}
+      price={'2 700 ₽/ 500 г.'}
+      urlIcon={'img/pluse_white.png'}
+      />
+
+      </div>
+      </div>
+    </main>
   );
 }
 
---
+++ src/components/card.js
@@ -0,0 +1,47 @@
+import React, {useRef} from 'react';
+import './card.css';
+
+
+
+
+function Card ({url, tittle, description, price}) {
+
+ /*    React.useEffect (() => {
+    let add = document.querySelector ('#addColorS')
+    let addColor = document.querySelector ('#notColor')
+
+    if (add.type == 'mouseover') {
+        add.target.display = 'none';
+        addColor.target.display = 'block';
+    } else (add.type == 'mouseout') {
+        add.target.display = 'block';
+        addColor.target.display = 'none';
+    }
+    })
+ */
+
+
+    return (
+    <div className="card">
+        <img className='card__preview' src={url} alt='' />
+
+        <h2 className='card__header'>
+        {tittle}
+        </h2>
+
+        <p className='card__description'>
+        {description}
+        </p>
+
+        <div className='card_price'>
+        {price}
+
+        <div> <button className='add'><b className='textButton'> + </b></button></div>
+        </div>
+    </div>
+    );
+  }
+
+
+
+  export default Card;
